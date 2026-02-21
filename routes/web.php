b345582e9cfd773231c4b25737708b1e517b3381
Tisha Khathurani <tishaak17@gmail.com>
2026-02-21 12:05:42 +0530

    feat: create endpoint to test job creation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\ProcessPayment;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/generate-jobs', function () {
+
+    for ($i = 0; $i < 15; $i++) {
+        ProcessPayment::dispatch();
+    }
+
+    return "15 jobs dispatched.";
 });
